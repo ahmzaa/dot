8143bba81e6eef4008a31277739b77f9f53a0de2
Hamza Ahmed <h.4hmz4@gmail.com>
2023-12-21 03:39:21 +0000

    Added LSP

--- nvim/.config/nvim/lua/ahmza/plugins.lua
+++ nvim/.config/nvim/lua/ahmza/plugins.lua
@@ -12,6 +12,16 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
+  -- LSP stuff
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  {'L3MON4D3/LuaSnip'},
+
+
   {'nvim-treesitter/nvim-treesitter'},
   {'lewis6991/gitsigns.nvim'},
   {'voldikss/vim-floaterm'},
