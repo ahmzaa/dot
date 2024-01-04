1cd217ca8bf40b1f5119710e165e451ed90038a8
Hamza Ahmed <h.4hmz4@gmail.com>
2024-01-04 17:35:34 +0000

    Fixed some keybindings

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,13 +1,16 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fs', function()
-    builtin.grep_string({ search = vim.fn.input('Grep > ') });
-end)
 
 require('telescope').setup{
   defaults = {
+    --layout_config = {
+    --    vertical = {width = 0.5}
+    --},
     -- Default configuration for telescope goes here:
     -- config_key = value,
     mappings = {
@@ -15,7 +18,7 @@ require('telescope').setup{
         -- map actions.which_key to <C-h> (default: <C-/>)
         -- actions.which_key shows the mappings for your picker,
         -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-        ["<C-h>"] = "which_key"
+        -- ["<C-h>"] = "which_key"
       }
     }
   },
